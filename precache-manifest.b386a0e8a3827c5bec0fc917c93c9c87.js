27e86250daf85a3891e8787cb07baeabdd88e4ec
Code Sandbox <hello@codesandbox.io>
2023-11-14 11:03:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b386a0e8a3827c5bec0fc917c93c9c87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6eb79de2046290a74dec18703bfa132",
+    "url": "https://inbavathy.github.io/csb-2t68d3/index.html"
+  },
+  {
+    "revision": "075fd640883c92ae1170",
+    "url": "https://inbavathy.github.io/csb-2t68d3/static/css/2.828d44ad.chunk.css"
+  },
+  {
+    "revision": "3f69a1c84724e8f04545",
+    "url": "https://inbavathy.github.io/csb-2t68d3/static/css/main.acab5b9a.chunk.css"
+  },
+  {
+    "revision": "075fd640883c92ae1170",
+    "url": "https://inbavathy.github.io/csb-2t68d3/static/js/2.a57ff1b7.chunk.js"
+  },
+  {
+    "revision": "3f69a1c84724e8f04545",
+    "url": "https://inbavathy.github.io/csb-2t68d3/static/js/main.0a130db8.chunk.js"
+  },
+  {
+    "revision": "5f2eb7eee875b5e9482e",
+    "url": "https://inbavathy.github.io/csb-2t68d3/static/js/runtime~main.4a3b8df3.js"
+  }
+]);
